f4cbab27db5806b2d85e914a65e3d3438d84e554
Shihab US <shihabudheen@bounceshare.com>
2020-05-02 13:15:41 +0530

    adding push notifications

--- sw.js
+++ sw.js
@@ -1,34 +1,82 @@
-const cacheName = 'cache-v1'
+/*
+*
+*  Push Notifications codelab
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
 
-const resourcesToPreCache = [
-  '/',
-  '/index.html',
-  '/main.css',
-  '/app.js'
-]
+/* eslint-env browser, serviceworker, es6 */
 
-// self is the SW
-self.addEventListener('install', e => {
-  console.log('SW Install event')
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(resourcesToPreCache)
+'use strict';
+
+/* eslint-disable max-len */
+
+const applicationServerPublicKey = 'BF2z9IPV0dSwKd0nJjYfSuqLyqBOKljRC-QZM0yeFUz4fi321u2gHrAW2i794pRiSgL08iCrm_4DqAFxe-igt9I';
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
     })
-  )
-})
-
-self.addEventListener('activate', e => {
-  console.log('activate e')
-})
-
-
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-    .then(cachedResponse => {
-      return cachedResponse || fetch(e.request)
-  }))
-})
-
-
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
+});
